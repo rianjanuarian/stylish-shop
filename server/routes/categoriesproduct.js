c9e047b4aeb3f4b47bff977b21f9b75296587ae6
Indra Oki Sandy <indraokisandy1@gmail.com>
2023-10-26 11:30:04 +0700

    Delete Juctions

--- server/routes/categoriesproduct.js
+++
@@ -1,8 +0,0 @@
-const categoriesProductRoutes = require("express").Router();
-const CategoriesProductControllers = require("../controllers/BrandController");
-
-
-
-
-
-module.exports = categoriesProductRoutes;
