9a6c3c5348b898d7c49f527d0f71fbf3cc7d6197
Indra Oki Sandy <indraokisandy1@gmail.com>
2023-10-26 09:36:52 +0700

    Update Routes

--- server/routes/transaction.js
+++ server/routes/transaction.js
@@ -1,8 +1,9 @@
 const transactionRoutes = require("express").Router();
 const TransactionControllers = require("../controllers/TransactionController");
 
-
-
-
+transactionRoutes.get("/approve", TransactionControllers.updateStatus);
+transactionRoutes.get("/", TransactionControllers.getTransaction);
+transactionRoutes.get("/:id", TransactionControllers.getTransactionDetail);
+transactionRoutes.post("/create", TransactionControllers.createTransaction);
 
 module.exports = transactionRoutes;
