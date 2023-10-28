74a7de499f62a927c8ad4e39453e16f1d7c7d683
andikatp <triprasetya_andika@yahoo.com>
2023-10-29 01:07:13 +0700

    changing throw error to next middleware

--- server/controllers/BrandController.js
+++ server/controllers/BrandController.js
@@ -1,41 +1,41 @@
 const { brand } = require("../models");
 
 class BrandControllers {
-  static async getBrands(req, res) {
+  static async getBrands(req, res, next) {
     try {
       const brands = await brand.findAll();
       res.status(200).json(brands);
     } catch (err) {
-      res.status(500).json({ message: err.message });
+      next(err)
     }
   }
 
-  static async create(req, res) {
+  static async create(req, res, next) {
     try {
       const { name, image } = req.body;
       const brand = await brand.create({ name, image });
       res.status(200).json(brand);
     } catch (err) {
-      res.status(500).json({ message: err.message });
+      next(err)
     }
   }
-  static async update(req, res) {
+  static async update(req, res, next) {
     try {
       const { id } = req.params;
       const { name, image } = req.body;
       const brand = await brand.update({ name, image }, { where: { id } });
       res.status(200).json(brand);
     } catch (err) {
-      res.status(500).json({ message: err.message });
+      next(err)
     }
   }
-  static async delete(req, res) {
+  static async delete(req, res, next) {
     try {
       const { id } = req.params;
       const brand = await brand.destroy({ where: { id } });
       res.status(200).json(brand);
     } catch (err) {
-      res.status(500).json({ message: err.message });
+      next(err)
     }
   }
 }
--- server/controllers/CategoriesControllers.js
+++ server/controllers/CategoriesControllers.js
@@ -1,26 +1,26 @@
 const { category } = require("../models");
 
 class CategorysControllers {
-  static async getCategories(req, res) {
+  static async getCategories(req, res, next) {
     try {
       const categories = await category.findAll();
       res.status(200).json(categories);
     } catch (err) {
-      res.status(500).json({ message: err.message });
+      next(err);
     }
   }
 
-  static async create(req, res) {
+  static async create(req, res, next) {
     try {
       const { name, image } = req.body;
       const category = await category.create({ name, image });
       res.status(200).json(category);
     } catch (err) {
-      res.status(500).json({ message: err.message });
+      next(err);
     }
   }
 
-  static async update(req, res) {
+  static async update(req, res, next) {
     try {
       const { id } = req.params;
       const { name, image } = req.body;
@@ -30,17 +30,17 @@ class CategorysControllers {
       );
       res.status(200).json(category);
     } catch (err) {
-      res.status(500).json({ message: err.message });
+      next(err);
     }
   }
 
-  static async delete(req, res) {
+  static async delete(req, res, next) {
     try {
       const { id } = req.params;
       const category = await category.destroy({ where: { id } });
       res.status(200).json(category);
     } catch (err) {
-      res.status(500).json({ message: err.message });
+      next(err);
     }
   }
 }
