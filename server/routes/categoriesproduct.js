a4c0bf524f56d08658d4f51febcc64588ae99b87
andikatp <triprasetya_andika@yahoo.com>
2023-10-30 01:00:14 +0700

    adding review controller and review route also adding categoriesproduct route

--- server/routes/categoriesproduct.js
+++ server/routes/categoriesproduct.js
@@ -1,5 +1,10 @@
 const categoriesProductRoutes = require("express").Router();
+const CategoriesProductControllers = require('../controllers/CategoriesControllers');
 
+categoriesProductRoutes.get('/', CategoriesProductControllers.getCategories);
+categoriesProductRoutes.post('/create', CategoriesProductControllers.create);
+categoriesProductRoutes.post('/update/:id', CategoriesProductControllers.update);
+categoriesProductRoutes.delete('/delete', CategoriesProductControllers.delete);
 
 
 
--- server/routes/review.js
+++ server/routes/review.js
@@ -2,7 +2,12 @@ const reviewRoutes = require("express").Router();
 const ReviewControllers = require("../controllers/ReviewController");
 
 
-
+reviewRoutes.get('/', ReviewControllers.getReviews);
+reviewRoutes.get('/:reviewId', ReviewControllers.getReview);
+reviewRoutes.get('/:id', ReviewControllers.getReviewByProduct);
+reviewRoutes.post('/create', ReviewControllers.createReview);
+reviewRoutes.delete('/delete/:id', ReviewControllers.deleteReview);
+reviewRoutes.post('/update/:id', ReviewControllers.updateReview);
 
 
 module.exports = reviewRoutes;
