036468d78c1cd548502372297ff767d4b6703069
Indra Oki Sandy <indraokisandy1@gmail.com>
2023-10-31 17:35:21 +0700

    adding verifyAdmin in courier

--- server/routes/courier.js
+++ server/routes/courier.js
@@ -1,9 +1,10 @@
 const courierRoutes = require("express").Router();
+const { verifyAdmin } = require("../middlewares/verifyRole");
 const CourierControllers = require("../controllers/CourierController");
 
-// courierRoutes.get("/", BrandControllers.getCouriers);
-// courierRoutes.post("/create", BrandControllers.create);
-// courierRoutes.put("/update/:id", BrandControllers.update);
-// courierRoutes.delete("/delete/:id", BrandControllers.delete);
+courierRoutes.get("/", CourierControllers.getCouriers);
+courierRoutes.post("/create", verifyAdmin, CourierControllers.create);
+courierRoutes.put("/update/:id", verifyAdmin, CourierControllers.update);
+courierRoutes.delete("/delete/:id", verifyAdmin, CourierControllers.delete);
 
 module.exports = courierRoutes;
