e6c3c5c12dee13ab02166a8a43628676c51e5f1e
Indra Oki Sandy <indraokisandy1@gmail.com>
2023-11-02 00:17:26 +0700

    Update Login with animated

--- cms/src/pages/Login/Login.js
+++ cms/src/pages/Login/Login.js
@@ -2,18 +2,30 @@ import React from "react";
 import "./login.css";
 const LoginPage = () => {
   return (
-    <div className="login-page">
-      <div className="login-container">
-        <div className="card">
-          <h1 className="textcenter">Login</h1>
-          <form>
-            <input type="text" placeholder="Username" />
-            <input type="password" placeholder="Password" />
-            <button className="btn-login" type="submit">
-              Login
-            </button>
-          </form>
-        </div>
+    <div className="main">
+      <input type="checkbox" id="chk" aria-hidden="true" />
+
+      <div className="signup">
+        <form>
+          <label for="chk" aria-hidden="true">
+            Sign up
+          </label>
+          <input type="text" name="username" placeholder="Username" required />
+          <input type="email" name="email" placeholder="Email" required />
+          <input type="Password" name="pswd" placeholder="Password" required />
+          <button>Sign up</button>
+        </form>
+      </div>
+
+      <div className="login">
+        <form>
+          <label for="chk" aria-hidden="true">
+            Login
+          </label>
+          <input type="email" name="email" placeholder="Email" required />
+          <input type="Password" name="pswd" placeholder="Password" required />
+          <button>Login</button>
+        </form>
       </div>
     </div>
   );
