941d48e2adf8a1971306d8228bbfa1e18df06ad4
Indra Oki Sandy <indraokisandy1@gmail.com>
2023-11-01 09:53:29 +0700

    Update User Controller and clean code

--- server/routes/user.js
+++ server/routes/user.js
@@ -10,21 +10,22 @@ userRoutes.post("/login_with_google", UserControllers.loginWithGoogle);
 
 //Admin Only
 userRoutes.get("/", verifyAdmin, UserControllers.getUser);
+userRoutes.post("/create_admin", UserControllers.createAdmin);
 userRoutes.post("/login_admin", UserControllers.loginAdmin);
-userRoutes.put("/change_password_admin", verifyAdmin, UserControllers.changePassword);
-userRoutes.post("/create_admin", verifyAdmin, UserControllers.createAdmin);
+userRoutes.put(
+  "/change_password_admin",
+  verifyAdmin,
+  UserControllers.changePassword
+);
 userRoutes.put("/update_admin/:id", verifyAdmin, UserControllers.updateAdminV2);
-userRoutes.delete("/delete_account/:id", verifyAdmin, UserControllers.deleteAccount);
+userRoutes.delete(
+  "/delete_account/:id",
+  verifyAdmin,
+  UserControllers.deleteAccount
+);
 
 //User Data
 userRoutes.put("/change_password", verifyUser, UserControllers.changePassword);
 userRoutes.put("/update", verifyUser, UserControllers.update);
-
-//bisa dibuang
-userRoutes.put("/update/:id", verifyAdmin, UserControllers.updateAdmin);userRoutes.post("/register", UserControllers.register);
-userRoutes.post("/login", UserControllers.login);
-userRoutes.post("/google_sign_in", UserControllers.signInWithGoogle);
-userRoutes.get("/logout", UserControllers.logout);
-
 
 module.exports = userRoutes;
