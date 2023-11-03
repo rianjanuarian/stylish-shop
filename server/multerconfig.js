9907de1473bcefabf57d3a5f386a54b4774f4509
rianjanuarian <reeyanrian@gmail.com>
2023-11-03 18:35:58 +0700

    multer fix

--- server/multerconfig.js
+++ server/multerconfig.js
@@ -1,15 +1,15 @@
 const multer = require("multer");
-const path = require("path");
 
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, "public/uploads"); // Simpan file di folder 'uploads/'
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.fieldname+ Date.now() + '-' + file.originalname); 
-    }
-});
 
-const upload = multer({ storage: storage });
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//         cb(null, "public/uploads"); // Simpan file di folder 'uploads/'
+//     },
+//     filename: function (req, file, cb) {
+//         cb(null, file.fieldname+ Date.now() + '-' + file.originalname); 
+//     }
+// });
+
+const upload = multer({dest: "public/uploads"});
 
 module.exports = upload;
