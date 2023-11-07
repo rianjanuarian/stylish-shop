971f11bc60846aed0ef23a36925c8f3a27cf642c
Indra Oki Sandy <indraokisandy1@gmail.com>
2023-11-07 23:40:56 +0700

    Recreate Login

--- cms/src/pages/Login/Login.js
+++ cms/src/pages/Login/Login.js
@@ -5,6 +5,14 @@ import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { login } from "../../redux/authSlice";
 
+// React Icons
+import { FaUser, FaLock } from "react-icons/fa";
+
+// Images
+import wave from "../../assets/images/wave.png";
+import bg from "../../assets/images/bg.svg";
+import avatar from "../../assets/images/avatar.svg";
+
 const LoginPage = () => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
@@ -14,6 +22,11 @@ const LoginPage = () => {
     email: "",
     password: "",
   });
+
+  console.log(formData);
+
+  const [isEmailFocused, setIsEmailFocused] = useState(false);
+  const [isPasswordFocused, setIsPasswordFocused] = useState(false);
 
   const handleChange = (e) => {
     const { id, value } = e.target;
@@ -26,6 +39,12 @@ const LoginPage = () => {
   const handleLogin = (e) => {
     e.preventDefault();
     dispatch(login(formData));
+  };
+
+  const remcl = (event, inputRef, setIsFocused) => {
+    if (event.target.value === "") {
+      setIsFocused(false);
+    }
   };
 
   useEffect(() => {
@@ -46,69 +65,68 @@ const LoginPage = () => {
   }, [auth, navigate]);
 
   return (
-    <div className="login-body">
-      <div className="main">
-        <input type="checkbox" id="chk" aria-hidden="true" />
-
-        <div className="login">
-          <form onSubmit={handleLogin}>
-            <label className="label-login" htmlFor="chk" aria-hidden="true">
-              Login
-            </label>
-            <input
-              className="input-login"
-              type="email"
-              name="email"
-              placeholder="Email"
-              id="email"
-              onChange={handleChange}
-              required
-            />
-            <input
-              className="input-login"
-              type="Password"
-              name="password"
-              placeholder="Password"
-              id="password"
-              onChange={handleChange}
-              required
-            />
-            <button className="button-login">Login</button>
-          </form>
+    <>
+      <img className="wave" src={wave} alt="Wave" />
+      <div className="container">
+        <div className="img">
+          <img src={bg} alt="Background" />
         </div>
-
-        <div className="signup">
-          <form>
-            <label className="label-login" htmlFor="chk" aria-hidden="true">
-              Sign Up
-            </label>
-            <input
-              className="input-login"
-              name="username"
-              placeholder="Username"
-              required
-            />
-            <input
-              className="input-login"
-              type="email"
-              name="email"
-              placeholder="Email"
-              required
-            />
-            <input
-              className="input-login"
-              type="Password"
-              name="pswd"
-              placeholder="Password"
-              required
-            />
-            <button className="button-login" type="submit">
-              Sign Up
+        <div className="login-content">
+          <form onSubmit={handleLogin}>
+            <img src={avatar} alt="Avatar" />
+            <h2 className="title">Welcome</h2>
+            <div className={`input-div one ${isEmailFocused ? "focus" : ""}`}>
+              <div className="i">
+                <i>
+                  <FaUser />
+                </i>
+              </div>
+              <div className="div">
+                <h5>Email</h5>
+                <input
+                  type="email"
+                  className="input"
+                  name="email"
+                  id="email"
+                  onChange={handleChange}
+                  onFocus={() => setIsEmailFocused(true)}
+                  onBlur={(e) => remcl(e, formData.email, setIsEmailFocused)}
+                  required
+                />
+              </div>
+            </div>
+            <div
+              className={`input-div pass ${isPasswordFocused ? "focus" : ""}`}
+            >
+              <div className="i">
+                <i>
+                  <FaLock />
+                </i>
+              </div>
+              <div className="div">
+                <h5>Password</h5>
+                <input
+                  type="password"
+                  className="input"
+                  name="password"
+                  id="password"
+                  onChange={handleChange}
+                  onFocus={() => setIsPasswordFocused(true)}
+                  onBlur={(e) =>
+                    remcl(e, formData.password, setIsPasswordFocused)
+                  }
+                  required
+                />
+              </div>
+            </div>
+            <a href="/">Forgot Password?</a>
+            <button type="submit" className="btn">
+              Login
             </button>
           </form>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
