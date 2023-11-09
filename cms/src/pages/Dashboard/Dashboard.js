cb1dd1e66f8d9a443d2d5dccfc860a22a07fd5be
Indra Oki Sandy <indraokisandy1@gmail.com>
2023-11-09 08:36:53 +0700

    Update Dashboard

--- cms/src/pages/Dashboard/Dashboard.js
+++ cms/src/pages/Dashboard/Dashboard.js
@@ -1,23 +1,61 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import sidebar_menu from "../../constants/sidebar-menu";
 import SideBar from "../../components/Sidebar/Sidebar";
 import DashboardHeader from "../../components/DashboardHeader";
+
+import { getUsers, userSelectors } from "../../redux/userSlice";
+import {
+  getTransactions,
+  transactionSelectors,
+} from "../../redux/transactionSlice";
+import { getProducts, selectAllProducts } from "../../redux/productSlice";
+import { useSelector, useDispatch } from "react-redux";
+
+// Import icons
+import profile from "../../assets/icons/user.svg";
+import order from "../../assets/icons/order.svg";
+import total_product from "../../assets/icons/total_product.svg";
 
 // CSS
 import "./Dashboard.css";
 
 // Chart
-import { Bar, Doughnut } from "react-chartjs-2";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+import { Doughnut } from "react-chartjs-2";
 import "chart.js/auto";
 
+ChartJS.register(ArcElement, Tooltip, Legend);
+
 const Dashboard = () => {
+  const dispatch = useDispatch();
+  // User
+  const users = useSelector(userSelectors.selectAll);
+  // Transaction
+  const transactions = useSelector(transactionSelectors.selectAll);
+  // Product
+  const products = useSelector(selectAllProducts);
+
+  useEffect(() => {
+    dispatch(getUsers());
+    dispatch(getTransactions());
+    dispatch(getProducts());
+  }, [dispatch]);
+
   const [chartData, setChartData] = useState({
-    labels: ["Total User", "Total Order"],
+    labels: ["Total User", "Total Order", "Total Product"],
     datasets: [
       {
         label: "Data",
-        data: [100, 250], // Replace with your actual data
-        backgroundColor: ["rgba(75, 192, 192, 0.6)", "rgba(255, 99, 132, 0.6)"],
+        data: [
+          parseInt(users.length),
+          parseInt(transactions.length),
+          parseInt(products.length),
+        ],
+        backgroundColor: [
+          "rgba(75, 192, 192, 0.6)",
+          "rgba(255, 99, 132, 0.6)",
+          "rgba(59, 77, 54, 0.2)",
+        ],
       },
     ],
   });
@@ -31,37 +69,28 @@ const Dashboard = () => {
           <div className="container2">
             <div className="card">
               <div className="card-title">
-                <img
-                  src="https://cdn-icons-png.flaticon.com/512/149/149071.png"
-                  alt="No image"
-                />
+                <img src={profile} alt="User" />
                 <div>
                   <h2>Total User</h2>
-                  <h3>200</h3>
+                  <h3>{users.length}</h3>
                 </div>
               </div>
             </div>
             <div className="card">
               <div className="card-title">
-                <img
-                  src="https://cdn-icons-png.flaticon.com/512/149/149071.png"
-                  alt="No image"
-                />
+                <img src={order} alt="Transaction" />
                 <div>
                   <h2>Total Order</h2>
-                  <h3>200</h3>
+                  <h3>{transactions.length}</h3>
                 </div>
               </div>
             </div>
             <div className="card">
               <div className="card-title">
-                <img
-                  src="https://cdn-icons-png.flaticon.com/512/149/149071.png"
-                  alt="No image"
-                />
+                <img src={total_product} alt="Product" />
                 <div>
-                  <h2>Visitor</h2>
-                  <h3>200</h3>
+                  <h2>Total Product</h2>
+                  <h3>{products.length}</h3>
                 </div>
               </div>
             </div>
