fc6b7a16197c3fd7658bf96b7f4a78c85436cd15
Indra Oki Sandy <indraokisandy1@gmail.com>
2023-11-16 10:48:48 +0700

    update vercel

--- server/multerconfig.js
+++ server/multerconfig.js
@@ -1,15 +1,14 @@
 const multer = require("multer");
 
-
 // const storage = multer.diskStorage({
 //     destination: function (req, file, cb) {
 //         cb(null, "public/uploads"); // Simpan file di folder 'uploads/'
 //     },
 //     filename: function (req, file, cb) {
-//         cb(null, file.fieldname+ Date.now() + '-' + file.originalname); 
+//         cb(null, file.fieldname+ Date.now() + '-' + file.originalname);
 //     }
 // });
 
-const upload = multer({dest: "public/uploads"});
+const upload = multer({ dest: "/tmp" });
 
 module.exports = upload;
