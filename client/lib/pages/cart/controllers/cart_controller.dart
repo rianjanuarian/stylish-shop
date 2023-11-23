d974f04cee21bb2bb3194206fa9116d34686d5bd
andikatp <triprasetya_andika@yahoo.com>
2023-11-23 14:05:29 +0700

    cart function

--- client/lib/pages/cart/controllers/cart_controller.dart
+++ client/lib/pages/cart/controllers/cart_controller.dart
@@ -1,6 +1,5 @@
 import 'package:client/routes/app_pages.dart';
 import 'package:dio/dio.dart';
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class CartController extends GetxController {
--- client/lib/pages/home_page/detail_product/controllers/detail_product_controller.dart
+++ client/lib/pages/home_page/detail_product/controllers/detail_product_controller.dart
@@ -1,12 +1,18 @@
 import 'package:client/models/products.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import '../../../../services/keys/get_storage_key.dart';
 
 class DetailProductController extends GetxController {
   RxInt quantity = 1.obs;
   RxInt price = 0.obs;
   Rx<Color> selectedColor = Colors.red.obs;
   int initialProductPrice = 200000;
+  RxBool isLoading = RxBool(false);
+  final dio = Dio();
+  final storage = GetStorage();
 
   void addQuantity() {
     quantity++;
@@ -27,6 +33,51 @@ class DetailProductController extends GetxController {
   void setInitialPrice(int initialPrice) {
     price.value = quantity.value * initialPrice;
     changePrice();
+  }
+
+  void addToCart(int productId) async {
+    try {
+      final token = await storage.read(GetStorageKey.token);
+      final res = await dio.post('https://stylish-shop.vercel.app/carts/create',
+          data: {
+            "productId": productId,
+            "color": getColorFromString(selectedColor),
+            "qty": quantity.value,
+            "total_price": price.value
+          },
+          options: Options(headers: {
+            'Authorization': 'Bearer $token',
+            'Content-Type': 'application/json',
+          }));
+      Get.snackbar('Success', res.data?['message']);
+      print(res);
+    } catch (e) {
+      if (e is DioException) {
+        final errorResponse = e.response;
+        if (errorResponse != null) {
+          final errorMessage = errorResponse.data?['message'];
+          Get.snackbar('Error', errorMessage ?? 'Unknown error');
+        } else {
+          Get.snackbar('Error', 'Unknown error occurred');
+        }
+        isLoading.toggle();
+      }
+    }
+  }
+
+  String getColorFromString(Rx<Color> color) {
+    switch (color) {
+      case Colors.red:
+        return "red";
+      case Colors.green:
+        return 'green';
+      case Colors.blue:
+        return 'blue';
+      case Colors.yellow:
+        return 'yellow';
+      default:
+        return 'white';
+    }
   }
 
   @override
