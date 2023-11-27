708b66d585019991c33d9513056b3ac5e5c04519
Indra Oki Sandy <indraokisandy1@gmail.com>
2023-11-27 10:36:27 +0700

    update minor and bug

--- client/lib/pages/transaction/views/place_order_view.dart
+++ client/lib/pages/transaction/views/place_order_view.dart
@@ -1,32 +1,42 @@
 import 'package:cached_network_image/cached_network_image.dart';
-import 'package:client/pages/transaction/controllers/place_order_controller.dart';
+import 'package:client/pages/cart/views/cart_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
+import 'package:intl/intl.dart';
+
+import '../../../services/api_service/courier/courier_models.dart';
+import '../../cart/controllers/cart_controller.dart';
+import '../../profile/setting/controllers/setting_controller.dart';
+import '../controllers/place_order_controller.dart';
 
 class PlaceOrderView extends GetView<PlaceOrderView> {
   const PlaceOrderView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final List order = [
-      {
-        'image':
-            'https://images.unsplash.com/photo-1542291026-7eec264c27ff?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-        'title': 'Roller Rabbit',
-        'description': 'Vado Odelle Dress',
-        'price': 198,
-        'quantity': 1,
-      },
-      {
-        'image':
-            'https://images.unsplash.com/photo-1542291026-7eec264c27ff?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-        'title': 'Axel Arigato',
-        'description': 'Clean 90 Triole Snakers',
-        'price': 245,
-        'quantity': 1,
-      },
-    ];
+    final controller = Get.put(PlaceOrderController());
+    final userController = Get.put(SettingController());
+    final cartController = Get.put(CartController());
+
+    // final List order = [
+    //   {
+    //     'image':
+    //         'https://images.unsplash.com/photo-1542291026-7eec264c27ff?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    //     'title': 'Roller Rabbit',
+    //     'description': 'Vado Odelle Dress',
+    //     'price': 198,
+    //     'quantity': 1,
+    //   },
+    //   {
+    //     'image':
+    //         'https://images.unsplash.com/photo-1542291026-7eec264c27ff?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    //     'title': 'Axel Arigato',
+    //     'description': 'Clean 90 Triole Snakers',
+    //     'price': 245,
+    //     'quantity': 1,
+    //   },
+    // ];
     return Scaffold(
       bottomNavigationBar: Container(
         height: 70.h,
@@ -46,7 +56,9 @@ class PlaceOrderView extends GetView<PlaceOrderView> {
                   style: TextStyle(fontWeight: FontWeight.bold),
                 ),
                 Text(
-                  '\$433.00',
+                  NumberFormat.currency(
+                          locale: 'id', symbol: 'Rp ', decimalDigits: 0)
+                      .format(cartController.totalPrice),
                   style:
                       TextStyle(fontWeight: FontWeight.bold, fontSize: 20.sp),
                 ),
@@ -85,51 +97,65 @@ class PlaceOrderView extends GetView<PlaceOrderView> {
                 style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18.sp),
               ),
               SizedBox(height: 20.h),
-              Container(
-                padding: REdgeInsets.all(10),
-                width: double.infinity,
-                height: 150.h,
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(10).r,
-                  color: const Color(0xFFD9D9D9),
-                ),
-                child: const Text(
-                  'Jl. Cempaka Putih No. 1, Jakarta Pusat bla bla bla bla bla',
-                ),
-              ),
-              // TextField(
-              //   maxLines: 5,
-              //   decoration: InputDecoration(
-              //     fillColor: const Color(0xFFD9D9D9),
-              //     filled: true,
-              //     border: OutlineInputBorder(
-              //       borderRadius: BorderRadius.circular(10).r,
-              //       borderSide: const BorderSide(
-              //         width: 0,
-              //         style: BorderStyle.none,
-              //       ),
-              //     ),
-              //   ),
-              // ),
+              userController.user.value?.address == null
+                  ? TextField(
+                      maxLines: 5,
+                      decoration: InputDecoration(
+                        fillColor: const Color(0xFFD9D9D9),
+                        filled: true,
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10).r,
+                          borderSide: const BorderSide(
+                            width: 0,
+                            style: BorderStyle.none,
+                          ),
+                        ),
+                      ),
+                    )
+                  : Container(
+                      padding: REdgeInsets.all(10),
+                      width: double.infinity,
+                      height: 150.h,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10).r,
+                        color: const Color(0xFFD9D9D9),
+                      ),
+                      child: Text(
+                        '${userController.user.value?.address}',
+                      ),
+                    ),
               SizedBox(height: 20.h),
               Text(
                 'Product Item',
                 style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18.sp),
               ),
               SizedBox(height: 20.h),
-              // ...order
-              //     .asMap()
-              //     .entries
-              //     .map((order) => CartItem(
-              //           cart: Cart()
-              //         ))
-              //     .toList(),
+              Obx(() {
+                if (cartController.isLoading.isTrue) {
+                  return const Center(child: CircularProgressIndicator());
+                } else {
+                  return Column(
+                    children: cartController.carts.map((cart) {
+                      return CartItem(
+                        cart: cart,
+                        controller: cartController,
+                      );
+                    }).toList(),
+                  );
+                }
+              }),
               Text(
                 'Courier',
                 style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18.sp),
               ),
               SizedBox(height: 20.h),
-              _Courier(),
+              Obx(() {
+                if (controller.isLoading.isTrue) {
+                  return const Center(child: CircularProgressIndicator());
+                } else {
+                  return _Courier(controller: controller);
+                }
+              }),
             ],
           ),
         ),
@@ -139,9 +165,15 @@ class PlaceOrderView extends GetView<PlaceOrderView> {
 }
 
 class _Courier extends StatelessWidget {
+  _Courier({required this.controller});
+
+  final PlaceOrderController controller;
+
   @override
   Widget build(BuildContext context) {
-    final controller = Get.put(PlaceOrderController());
+    final Courier courier =
+        controller.couriers.isNotEmpty ? controller.couriers[0] : Courier();
+
     return Container(
       height: 70.h,
       padding: REdgeInsets.symmetric(horizontal: 20),
@@ -163,7 +195,7 @@ class _Courier extends StatelessWidget {
           Row(
             children: [
               CachedNetworkImage(
-                imageUrl: controller.couriers[0].image ?? '',
+                imageUrl: 'https://storage.googleapis.com/${courier.image}',
                 width: 80.w,
                 height: 80.h,
                 fit: BoxFit.cover,
@@ -171,14 +203,21 @@ class _Courier extends StatelessWidget {
                     const Center(child: CircularProgressIndicator()),
                 errorWidget: (context, url, error) => const Icon(Icons.error),
               ),
+              SizedBox(width: 20.w),
               Column(
                 mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Text(
-                    controller.couriers[0].name ?? '',
+                    '${courier.name}',
                     style: const TextStyle(fontWeight: FontWeight.bold),
                   ),
-                  const Text('\$15')
+                  Text(
+                    NumberFormat.currency(
+                            locale: 'id', symbol: 'Rp ', decimalDigits: 0)
+                        .format(courier.price),
+                    style: TextStyle(fontSize: 16.sp),
+                  ),
                 ],
               ),
             ],
