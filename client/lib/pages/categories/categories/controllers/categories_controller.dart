8abf90ad8c8d4af3baf8612091c0d44dee17c05a
Indra Oki Sandy <indraokisandy1@gmail.com>
2023-11-27 10:34:06 +0700

    delete unused code

--- client/lib/pages/categories/categories/controllers/categories_controller.dart
+++ client/lib/pages/categories/categories/controllers/categories_controller.dart
@@ -1,27 +1,21 @@
-
 import 'package:client/models/categories.dart';
 import 'package:client/routes/app_pages.dart';
 import 'package:dio/dio.dart';
 import 'package:get/get.dart';
 
 class CategoriesController extends GetxController {
-  
-var categoryId = <Categories>[].obs;
+  var categoryId = <Categories>[].obs;
   var categoriesList = <Categories>[].obs;
   var isLoading = true.obs;
-  @override
-  void onInit() {
-    super.onInit();
 
-    getCategories();
-  }
   Future<void> getCategories() async {
     String url = 'https://stylish-shop.vercel.app/categories';
     try {
       final response = await Dio().get(url);
       if (response.statusCode == 200) {
         final List<dynamic> result = response.data;
-        categoriesList.value = result.map((e) => Categories.fromJson(e)).toList();
+        categoriesList.value =
+            result.map((e) => Categories.fromJson(e)).toList();
         isLoading.value = false;
         update();
       } else {
@@ -29,7 +23,7 @@ var categoryId = <Categories>[].obs;
             'error fetching data', 'error : ${response.statusCode.toString()}');
       }
     } catch (e) {
-        if (e is DioException) {
+      if (e is DioException) {
         final errorResponse = e.response;
         if (errorResponse != null) {
           final errorMessage = errorResponse.data?['message'];
@@ -42,18 +36,18 @@ var categoryId = <Categories>[].obs;
     }
   }
 
- Future<void> getCategoryById(int id) async{
-  String url= 'https://stylish-shop.vercel.app/products/category/$id';
-  try {
-    final response = await Dio().get(url);
-    if (response.statusCode == 200) {
-      final Map<String,dynamic> data = response.data;
-      categoryId.value = [Categories.fromJson(data)];
-      isLoading.value = false;
-      update();
-    }
-  } catch (e) {
-    if (e is DioException) {
+  Future<void> getCategoryById(int id) async {
+    String url = 'https://stylish-shop.vercel.app/products/category/$id';
+    try {
+      final response = await Dio().get(url);
+      if (response.statusCode == 200) {
+        final Map<String, dynamic> data = response.data;
+        categoryId.value = [Categories.fromJson(data)];
+        isLoading.value = false;
+        update();
+      }
+    } catch (e) {
+      if (e is DioException) {
         final errorResponse = e.response;
         if (errorResponse != null) {
           final errorMessage = errorResponse.data?['message'];
@@ -63,12 +57,16 @@ var categoryId = <Categories>[].obs;
         }
         isLoading.value = true;
       }
+    }
   }
- }
 
   void goToSearch() {
     Get.toNamed(AppPages.search);
   }
 
- 
+  @override
+  void onInit() {
+    getCategories();
+    super.onInit();
+  }
 }
