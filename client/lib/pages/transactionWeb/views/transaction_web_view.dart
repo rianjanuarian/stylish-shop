5ab7c4ed23c636be67f6df601041299765c9b062
andikatp <triprasetya_andika@yahoo.com>
2023-11-30 15:44:02 +0700

    hapus back

--- client/lib/pages/transactionWeb/views/transaction_web_view.dart
+++ client/lib/pages/transactionWeb/views/transaction_web_view.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 import 'package:webview_flutter/webview_flutter.dart';
 import '../controllers/transaction_web_controller.dart';
@@ -8,19 +7,12 @@ class TransactionWebView extends GetView<TransactionWebController> {
   const TransactionWebView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return PopScope(
-      canPop: false,
+    return WillPopScope(
+      onWillPop: () async {
+        controller.goBack();
+        return false;
+      },
       child: Scaffold(
-        appBar: AppBar(
-          leadingWidth: 80.w,
-          leading: IconButton(
-            icon: const Icon(Icons.arrow_back),
-            onPressed: () => controller.goBack(),
-            style: IconButton.styleFrom(
-                backgroundColor: Colors.white, foregroundColor: Colors.black),
-          ),
-          backgroundColor: const Color(0xFF002755),
-        ),
         body: WebViewWidget(
           controller: controller.webController,
         ),
